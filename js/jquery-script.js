8264b725aefe2bfd9e8d8c307a5ee1b4fa14de93
RenataLinCastro <renata.lincastro@gmail.com>
2024-03-10 02:02:25 -0300

    Parte Final
    Projeto concluído previamente.
    ajustar o desaparecimento do formulário.

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,13 @@
 //efeito de esconder formulário de cadastro
+
+$(document).ready(function(){   //fórmula padrão para iniciar o jQuery
+    
+    //document.getElementById("botao-cadastrar").click()
+    $("#botao-cadastrar").click(function() {
+        $('#form-cadastrar').slideToggle('slow');
+        $('#section-login').slideToggle('slow');
+        $('#botao-cadastrar').hide();
+
+    })
+
+});
